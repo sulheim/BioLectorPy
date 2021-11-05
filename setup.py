3993b5021d52c9ecdc7390de640df84feec3fa19
Snorre Sulheim <ssulheim@gmail.com>
2021-11-05 13:56:13 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='BioLectorPy',
-    version='0.1.2',    
+    version='0.2.0',    
     description='A Python package for plotting BioLector results',
     long_description=long_description,
     long_description_content_type="text/markdown",
